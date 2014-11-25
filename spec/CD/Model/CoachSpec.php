d75490f0dad1a632f0292ca0713d0332213b71a5
michael.schramm <michael.schramm@gmail.com>
2014-11-25 20:32:02 +0100

    test 2 (phpspec is evil...)

--- spec/CD/Model/CoachSpec.php
+++ spec/CD/Model/CoachSpec.php
@@ -15,4 +15,8 @@ class CoachSpec extends ObjectBehavior
     function it_has_seats(){
         $this->hasSeats(4)->shouldReturn(true);
     }
+
+    function it_has_seats_valid(){
+        $this->shouldThrow(new \Exception())->during('hasSeats', array("4.2<"));
+    }
 }
--- src/CD/Model/Coach.php
+++ src/CD/Model/Coach.php
@@ -7,6 +7,10 @@ class Coach
     private $seats = 80;
     public function hasSeats($argument1)
     {
+        if(!is_int($argument1)){
+            throw new \Exception();
+        }
+
         return $this->seats>=$argument1;
     }
 }
